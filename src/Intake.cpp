76786377110cf2718b970920c2b746d2435dda18
No1Suprememe <hungryleo9316@gmail.com>
2018-04-26 15:04:38 -0700

    added intake files

---
+++ src/Intake.cpp
@@ -0,0 +1,22 @@
+/*
+ * Intake.cpp
+ *
+ *  Created on: Apr 26, 2018
+ *      Author: Admin
+ */
+#include "Intake.h"
+
+Intake::Intake()
+{
+
+}
+
+Intake::~Intake()
+{
+
+}
+
+void Intake::intakeBall()
+{
+
+}
---
+++ src/Intake.h
@@ -0,0 +1,29 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Apr 26, 2018
+ *      Author: Admin
+ */
+
+#ifndef SRC_INTAKE_H_
+#define SRC_INTAKE_H_
+
+#include "Macros.h"
+#include "ctre/Phoenix.h"
+#include "WPILib.h"
+
+class Intake
+{
+public:
+	Intake();
+	~Intake();
+
+	void intakeBall();
+
+private:
+
+
+};
+
+
+#endif /* SRC_INTAKE_H_ */
